6a78cc439d34b6d4be16a027b2561a6a5c4f618e
omariabdelhadi <omariabdelhadi09@gmail.com>
2026-05-10 22:00:47 +0100

    Premier commit:la creation de projet Spring Boot avec les entités JPA

---
+++ src/main/java/com/abdelhadielomari/banck/BanckAbdelhadiElomariApplication.java
@@ -0,0 +1,13 @@
+package com.abdelhadielomari.banck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanckAbdelhadiElomariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanckAbdelhadiElomariApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abdelhadielomari/banck/BanckAbdelhadiElomariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdelhadielomari.banck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanckAbdelhadiElomariApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
